2485a36ca191e5cc4f55fd2732d7281d92eb704e
chel12 <sema.chel12@gmail.com>
2023-12-22 21:54:13 +0300

    форма  после отправки + анимация

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -1,13 +1,20 @@
 import styles from './Home.module.css';
+const isSuccess = false;
 
 function Home() {
 	return (
 		<div className={styles.wrapper}>
 			<form>
-				<h1>Русы против Ящеров - Оставь заявку</h1>
-				<input type="name" placeholder="Введите имя:" />
-				<input type="email" placeholder="Введите email:" />
-				<button>Ящеры не захватят Русь!</button>
+				{isSuccess ? (
+					<div className={styles.success}>Форма отправлена</div>
+				) : (
+					<>
+						<h1>Русы против Ящеров - Оставь заявку</h1>
+						<input type="name" placeholder="Введите имя:" />
+						<input type="email" placeholder="Введите email:" />
+						<button>Ящеры не захватят Русь!</button>
+					</>
+				)}
 			</form>
 		</div>
 	);
