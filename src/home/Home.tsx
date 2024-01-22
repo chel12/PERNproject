829b637bd7f684953fee2cb0c27f1a12ee5ab241
chel12 <sema.chel12@gmail.com>
2024-01-22 20:15:09 +0300

    доделаны кнопки статуса и саксеса + бек доделан и запросы на локальну бд

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -1,18 +1,54 @@
 import styles from './Home.module.css';
-const isSuccess = false;
+import IFormState from './../Interface/interface';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { useState } from 'react';
 
 function Home() {
+	const { register, handleSubmit, reset } = useForm<IFormState>();
+	//тип из реакт хук форм
+	const [isLoading, setIsLoading] = useState(false);
+	const [isSuccess, setIsSuccess] = useState(false);
+	const submit: SubmitHandler<IFormState> = (data) => {
+		setIsLoading(true);
+		fetch('http://localhost:5000/api', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(data),
+		}).then((response) =>
+			response
+				.json()
+				.then((data) => {
+					if (!data) return;
+					setIsSuccess(true), reset();
+				})
+				.finally(() => {
+					setIsLoading(false);
+				})
+		);
+	};
 	return (
 		<div className={styles.wrapper}>
-			<form>
+			<form onSubmit={handleSubmit(submit)}>
 				{isSuccess ? (
 					<div className={styles.success}>Форма отправлена</div>
 				) : (
 					<>
 						<h1>Русы против Ящеров - Оставь заявку</h1>
-						<input type="name" placeholder="Введите имя:" />
-						<input type="email" placeholder="Введите email:" />
-						<button>Ящеры не захватят Русь!</button>
+						<input
+							type="name"
+							placeholder="Введите имя:"
+							{...register('name')}
+						/>
+						<input
+							type="email"
+							placeholder="Введите email:"
+							{...register('email')}
+						/>
+						<button disabled={isLoading}>
+							{isLoading ? `Загрузка` : `Хочу тоже бить ящеров!`}
+						</button>
 					</>
 				)}
 			</form>
